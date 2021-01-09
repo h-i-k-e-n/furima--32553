4416ff9d9eefd934ab103870d1711a1fd422adff
h-i-k-e-n <75025826+h-i-k-e-n@users.noreply.github.com>
2021-01-10 04:13:32 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
-def index
-end
+
+  def index
+@items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to controller: :items, action: :index
+    else
+      render "new"
+    end
+    
+  end
+
+
 end
