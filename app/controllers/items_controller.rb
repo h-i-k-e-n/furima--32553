875e3e374fced43c0eace7803f59e5770fe3ec81
h-i-k-e-n <75025826+h-i-k-e-n@users.noreply.github.com>
2021-01-16 12:17:54 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
   
-  def edit
-    @item = Item.find(params[:id])
-  end
+  # def edit
+  #   @item = Item.find(params[:id])
+  # end
 
   def update
 
