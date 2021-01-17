d25e4e7b5cc0540cb56f1512256729310576106a
h-i-k-e-n <75025826+h-i-k-e-n@users.noreply.github.com>
2021-01-17 11:44:25 +0900

    追記

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -13,6 +13,6 @@ end
 
 private
 def comment_params
-  params.require(:consumer).permit(:price).merge(user_id: current_user.id, item_id: params[:item_id])
+  params.require(:consumer).permit(:postal_code, :city, :address, :building_name, :tel_number, :address_id, :order_id ).merge(user_id: current_user.id, item_id: params[:item_id])
 end
 end
