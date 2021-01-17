16aa518137042855ce6a771ca0bddd9f37048307
h-i-k-e-n <75025826+h-i-k-e-n@users.noreply.github.com>
2021-01-17 10:49:38 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,8 @@ class ItemsController < ApplicationController
   
 
   def destroy 
-     @item.destroy
      redirect_to root_path unless current_user == @item.user
+     @item.destroy
   end
 
   private
